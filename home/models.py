3e36196e2db05070dc1698edf2ab042556ed4232
elixez9 <a1381ahmadi@gmail.com>
2024-12-17 18:39:53 +0330

    v3

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import SlugField
 
-# Create your models here.
+
+class post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    slug: SlugField = models.SlugField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.slug
